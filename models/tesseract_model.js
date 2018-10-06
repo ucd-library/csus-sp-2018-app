f8e422415c03fc5551e54c8a4730f5dfefd255bc
derekmaggio <derek.j.maggio@gmail.com>
2018-10-06 14:55:18 -0700

    ldp_controller.js : Pseudo code for getting from LDP and error handling it
    tesseract_controller.js : Pseudo code for sending data to tesseract and receiving the response
    
    data_structure_class.js : Added comment of datastructure for Eshan to create
    
    ldp_model.js Added todo
    
    tesseract_model.js : Added pseudo code for sending to ldp and getting back the response
    
    server.js : Changed a comment

--- models/tesseract_model.js
+++ models/tesseract_model.js
@@ -1,12 +1,10 @@
-
 const joi = require('joi')
 const request = require('request')
-
 
 exports.get = function(user, text, cb) {
     const options = {
         url: 'http://localhost:3000/fcrepo/rest/collection/example_3-catalogs/catalogs/199/media/images/199-3/svc:tesseract/full/full/0/default.jpg',
-        method: 'GET',
+        method: 'POST',
         headers: {
             'Accept': 'application/hocr+xml',
             'Accept-Charset': 'utf-8',
@@ -14,7 +12,23 @@ exports.get = function(user, text, cb) {
         }
     };
 
+    // Call request api. Pass the options variable.
+    // The return of the function is going be
+    //  err: if true, there was an error
+    //  res: headers, other metadata
+    //  body: the body of the HTTP request
     request(options, function(err, res, body) {
-        console.log(res)
+        tesseract_data = body
+
+        //todo create data parser function called parse_tesseract_data(data) ---> parsed_data
+
+        parsed_data = parse_tesseract_data(tesseract_data)
+
+        return parsed_data
     });
+}
+
+function parse_tesseract_data(data) {
+    //parse data with package
+    return parsed_data
 }
