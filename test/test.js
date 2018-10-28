b515527b78926cb02a61ca515ca6254cce53a895
Ehsan Khaligh <khalighehsan@gmail.com>
2018-10-28 15:01:11 -0700

    Installed chai

---
+++ test/test.js
@@ -0,0 +1,33 @@
+/*
+  Written by ehsan:
+
+
+  documentations:
+
+  chai: https://www.chaijs.com/
+  mocha: https://mochajs.org/
+
+  https://www.youtube.com/watch?v=MLTRHc5dk6s
+
+  to install:
+
+  npm install mocha
+  npm install chai
+
+  Added "test": "mocha"  in pachage.json
+  to run: npm test or npm run test -s
+  -s hides unnecessary
+
+*/
+
+//Exmple:
+/*
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
+*/
