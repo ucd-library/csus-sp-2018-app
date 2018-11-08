d65202939ab1944decbdbfcd555f2dca7a521ff4
CHas <ctlevinsky@gmail.com>
2018-11-08 14:52:58 -0800

    cleaned up comments

--- client/public/src/my-app.js
+++ client/public/src/my-app.js
@@ -113,10 +113,7 @@ class MyApp extends PolymerElement {
               .setLatLng(this._bounds._northEast)
               .setContent('<p><button type="button" onclick="post(box)">Send To Tesseract</button></p>')
               .openOn(map);
-          console.log(coords);
-          console.log(box);
-          console.log(this);
-          console.log("id: " + this.feature.properties.id);
+          console.log("Box ID: " + this.feature.properties.id);
       };
 
 
@@ -130,7 +127,17 @@ class MyApp extends PolymerElement {
           let height = Math.round(ne['lat'] - se['lat']);
           let pixel = [Math.round(nw['lng']), Math.round(imgH - nw['lat'])];
 
-          return [width,  height,  pixel];
+          //return [width,  height,  pixel];
+
+          return {
+              _image_path: img_loc,
+              _box_x_loc: pixel[0],
+              _box_y_loc: pixel[1],
+              _box_width: width,
+              _box_height: height,
+              _rotation_angle: 0
+          }
+
       }
 
 
@@ -138,7 +145,7 @@ class MyApp extends PolymerElement {
           boxes.forEach(box => draw_to_map(box))
       }
 
-      
+
       function draw_to_map(box){
           var p1 = [box._tesseract_request._box_y_loc,
               box._tesseract_request._box_x_loc
@@ -150,7 +157,6 @@ class MyApp extends PolymerElement {
           var layer = L.rectangle(box_bounds, {color: "red", weight: 1});
 
           var feature = layer.feature = layer.feature || {};
-
           feature.type = feature.type || "Feature";
           var props = feature.properties = feature.properties || {};
           props.id = box._box_id;
@@ -175,8 +181,6 @@ class MyApp extends PolymerElement {
           xhr.send(data);
       }
 
-
-      //Todo: Add locations to hold box_list and/or LDP data
       //tests data
       var ldp_data = {
           "user": "sample user",
@@ -223,13 +227,6 @@ class MyApp extends PolymerElement {
       display_boxes(box_list);
 
 
-
-      //Todo: allow leaflet-draw to interact with imported boxes
-
-
-      //Todo: attach box_id to boxes
-
-
       //Todo: write GET request
 
 
@@ -239,142 +236,6 @@ class MyApp extends PolymerElement {
       //Todo: write Post request
 
 
-
-
-
-/*
-
-      //get specify nothing
-      //delete send "box_id" : #
-      //put "box_id" of box to edit, tesseract data in JSON of options to edit
-      var map = L.map(this.$.map, {
-          drawControl: false,
-          zoomSnap: .25,
-          minZoom: -3.5,
-          crs: L.CRS.Simple
-      });
-
-      const iiif_svc = 'svc:iiif/full/full/0/default.jpg';
-      const img_host = 'http://localhost:3000/fcrepo/rest/';
-
-      var img_loc = 'collection/example_3-catalogs/catalogs/199/media/images/199-3';
-      var src = img_host + img_loc + '/' + iiif_svc;
-
-      // Create new image  object, this will allow the pre-caching of the image before it loads into leaflet
-      // This allows us to take the dimensions of the image and use them to define the bounds of the leaflet "map"
-      var img = new Image();
-      var imgH, imgW = 0;
-      img.addEventListener("load",function(){
-          imgH = this.naturalHeight;
-          imgW = this.naturalWidth;
-          console.log("height: " + imgH + " width: " + imgW);
-          var bounds = [[0,0], [imgH, imgW]];
-          var image = L.imageOverlay(src, bounds).addTo(map);
-          map.fitBounds(bounds);
-      });
-      img.src = src;
-
-
-      // Add the leaflet draw API to the image to allow the creation of boxes
-      var drawnItems = new L.FeatureGroup();
-      map.addLayer(drawnItems);
-      var drawControl = new L.Control.Draw({
-          draw: {
-              polygon: false,
-              marker: false,
-              circle: false,
-              polyline: false,
-              circlemarker: false
-          },
-          edit: {
-              featureGroup: drawnItems
-          }
-      });
-      map.addControl(drawControl);
-
-
-      // Send created JSON object to server to be run in tesseract
-      // for now, print response to console
-      function post(data){
-          let xhr = new XMLHttpRequest();
-
-          xhr.open("POST", 'tesseract', true);
-          xhr.setRequestHeader("Content-Type", "application/json");
-          xhr.onreadystatechange = function () {
-              if (xhr.readyState === 4 && xhr.status === 200) {
-                  console.log(this.responseText);
-                  var newWin = open('url','windowName','height=300,width=300');
-                  newWin.document.write(this.responseText);
-              }
-          };
-          xhr.send(data);
-      }
-
-
-      var clickBox = function(event) {
-          var coords = this.getLatLngs();
-          var box = JSON.stringify(convert(coords[0]));
-          post(box);
-          var popup = L.popup()
-              .setLatLng(this._bounds._northEast)
-              .setContent('<p><button type="button" onclick="post(box)">Send To Tesseract</button></p>')
-              .openOn(map);
-      };
-
-
-      // When a box is drawn on the image, same the box info (corners, dimensions, etc) create JSON object and
-      // send to tesseract
-      map.on('draw:created', function(e) {
-          var type = e.layerType,
-              layer = e.layer;
-          layer.on('click', clickBox);
-          drawnItems.addLayer(layer);
-      });
-
-
-      // Convert the coordinates generated by leaflet Draw into a format usable by tesseract
-      // called in "map.on('draw:created', function(e)"
-      function convert(coords){
-          // lat = x, lon = y
-          // rectangle drawn clockwise starting with south west corner
-          // points need to be scaled up by 10 to equate to pixels
-          let sw = upscale(coords[0]);
-          let nw = upscale(coords[1]);
-          let ne = upscale(coords[2]);
-          let se = upscale(coords[3]);
-
-          // Round values to remove any decimals, which would confuse tesseract
-          let wdth = Math.round(se[0] - sw[0]);
-          let hght = Math.round(ne[1] - se[1]);
-
-          // leaflet calculates y as distance from bottom
-          // tesseract y is distance from top
-          // origin y = img height - northern most y coordinate
-          let x_loc = Math.round(nw[0]);
-          let y_loc = Math.round(imgH - nw[1]);
-
-          let data_to_return = {
-              image_path:img_loc,
-              box_x_loc:x_loc,
-              box_y_loc:y_loc,
-              box_width:wdth,
-              box_height:hght,
-              rotation_angle:0
-
-          };
-
-          console.log(data_to_return);
-
-
-          return data_to_return
-      }
-
-      // scales the coordinates from latLon to pixels
-      function upscale(latLon) {
-          return [latLon["lng"] * scaler, latLon["lat"] * scaler]
-      }
-
-   */
   }
 
 }
